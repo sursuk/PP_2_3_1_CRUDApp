08dc91a84f58a242c6fa4276173dbc8dce099b49
sursuk <sursuk@users.noreply.github.com>
2022-07-05 07:28:57 +0300

    убран трансзакцион в слое дао

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -13,28 +13,28 @@ public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
     private EntityManager entityManager;
-    @Transactional
+
     @Override
     public List<User> getAllUsers() {
         return entityManager.createQuery("select u from User u", User.class).getResultList();
     }
 
-    @Transactional
+
     @Override
     public User getUserById(long id) {
         return entityManager.find(User.class, id);
     }
-    @Transactional
+
     @Override
     public void addUser(User user) {
         entityManager.persist(user);
     }
-    @Transactional
+
     @Override
     public void removeUser(long id) {
         entityManager.remove(getUserById(id));
     }
-    @Transactional
+
     @Override
     public void updateUser(User user) {
         entityManager.merge(user);
